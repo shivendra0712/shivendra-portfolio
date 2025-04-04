b1114567647bd76a13b851c981803f5bccb45f50
shivendra0712 <patelshivendra817@gmail.com>
2025-04-05 02:07:41 +0530

    fixed bug

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 const toggleBtn = document.getElementById("themeToggle");
 const lightIcon = document.querySelector(".lightIcon");
 const darkIcon = document.querySelector(".darkIcon");
+const lightBox = document.querySelector(".lightBox");
+const darkBox = document.querySelector(".darkBox");
 const body = document.body;
 
 toggleBtn.addEventListener("click", () => {
@@ -12,11 +14,15 @@ toggleBtn.addEventListener("click", () => {
         body.classList.add("bg-[rgb(227,227,227)]", "text-black");
         lightIcon.classList.remove("hidden");
         darkIcon.classList.add("hidden");
+        lightBox.classList.remove("hidden");
+        darkBox.classList.add("hidden");
     } else {
         body.classList.add("dark", "bg-gray-900", "text-white");
         body.classList.remove("bg-[rgb(227,227,227)]", "text-black");
         lightIcon.classList.add("hidden");
         darkIcon.classList.remove("hidden");
+        lightBox.classList.add("hidden");
+        darkBox.classList.remove("hidden");
     }
 });
 
